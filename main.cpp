fe4b1688553c1bc030ecaba7b27ade3cada95bf5
mgering <mgering@mail.de>
2025-04-14 15:53:30 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 int main() {
 	try {
 		Bureaucrat a("Alpha", 150);
+		Bureaucrat b("Beta", 42);
 		a.decrementGrade();
 		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+	}
+	catch (const std::exception &err) {
+		std::cerr << "Exception caught: "<< err.what() << std::endl;
+	}
+	try {
+		Bureaucrat c("Gamma", 42);
+		std::cout << c << std::endl;
 	}
 	catch (const std::exception &err) {
 		std::cerr << "Exception caught: "<< err.what() << std::endl;
